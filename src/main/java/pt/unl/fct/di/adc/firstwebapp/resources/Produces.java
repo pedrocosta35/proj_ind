ecdb96cab5b31421ad418ed34756176371134647
Pedro Costa <pcosta.pico@gmail.com>
2026-03-24 20:32:54 +0000

    Op1 missed import

--- src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/Produces.java
+++
@@ -1,7 +0,0 @@
-package pt.unl.fct.di.adc.firstwebapp.resources;
-
-public @interface Produces {
-
-  String value();
-
-}
--- src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/RegisterResource.java
+++ src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/RegisterResource.java
@@ -7,6 +7,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
